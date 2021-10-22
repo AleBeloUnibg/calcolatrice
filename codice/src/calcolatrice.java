ce8a49d91a8625aa05f55884d2a5c732bc8f868b
jaysalvi99 <andreasalvi99@gmail.com>
2021-10-22 11:41:55 +0200

    Update calcolatrice.java

--- codice/src/calcolatrice.java
+++ codice/src/calcolatrice.java
@@ -3,7 +3,11 @@ public class calcolatrice {
 
 	public static void main(String[] args) {
 		System.out.println("calculator");
-
+		ciao();
+	}
+	
+	public static void ciao() {
+		System.out.println("ahoooo");
 	}
 
 }

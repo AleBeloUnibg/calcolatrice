16534c55a157015f12601b85f3560865b4ee6907
jaysalvi99 <andreasalvi99@gmail.com>
2021-10-22 11:58:31 +0200

    oo

--- codice/src/calcolatrice.java
+++ codice/src/calcolatrice.java
@@ -1,7 +1,9 @@
-
+import prog.io.ConsoleOutputManager;
 public class calcolatrice {
 
 	public static void main(String[] args) {
+		ConsoleOutputManager out= new ConsoleOutputManager();
+		out.println("grazie di tutto cape");
 		System.out.println("provaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		ciao();
 		ciaoa();

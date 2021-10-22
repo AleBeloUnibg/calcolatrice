9d0fa9ca7b77cead026c99e50218deb94b352123
LucaCapelli <l.capelli11@studenti.unibg.it>
2021-10-22 11:53:16 +0200

    si

--- codice/src/calcolatrice.java
+++ codice/src/calcolatrice.java
@@ -4,10 +4,15 @@ public class calcolatrice {
 	public static void main(String[] args) {
 		System.out.println("calculator");
 		ciao();
+		ciaoa();
 	}
 	
 	public static void ciao() {
 		System.out.println("ahoooo");
+	}
+
+	public static void ciaoa() {
+		System.out.println("aa");
 	}
 
 }
